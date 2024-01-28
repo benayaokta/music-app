6eb634a8a46dc456eae068d5cda464dfc3d7a5e8
Benaya Oktavianus <53290795+benayaokta@users.noreply.github.com>
2024-01-28 19:23:05 +0700

    refactor base

--- BCAMusicPlayer/Common/Base/BaseViewController.swift
+++ BCAMusicPlayer/Common/Base/BaseViewController.swift
@@ -11,6 +11,22 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        setupNavigationBar()
+        
+    }
+    
+    private func setupNavigationBar() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.standardAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+
+        self.navigationItem.compactAppearance = appearance
+        self.navigationItem.standardAppearance = appearance
+        self.navigationItem.scrollEdgeAppearance = appearance
     }
 }
